ca5ad334f480121eb7d7c53d2102f58f52b65263
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-18 20:22:17 +0300

    React redux connect

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
       super(props);
 
       this.state = { term: '' };
       //we are binding function to this.
       this.onInputChange = this.onInputChange.bind(this);
+      this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){
-      console.log(event.target.value);
       //we have to bind this
       this.setState({term: event.target.value})
     }
 
     onFormSubmit(event){
       event.preventDefault();
+      //fetch weather
+
+      this.props.fetchWeather(this.state.term);
+      this.setState({term: ''});
     }
 
     render() {
@@ -35,3 +42,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
